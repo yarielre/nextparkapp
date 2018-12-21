4a471b22dc56f9c3fff122efafa819a775a75282
Yariel Rodriguez <yariel.re@wisegar.ch>
2018-12-21 11:02:06 +0100

    Updated IParkingDataService interface.

--- Nextpark/NextPark.Mobile/Services/Data/IParkingDataService.cs
+++ Nextpark/NextPark.Mobile/Services/Data/IParkingDataService.cs
@@ -6,7 +6,9 @@ namespace NextPark.Mobile.Services
 {
     public interface IParkingDataService
     {
+        Task<ParkingModel> Delete(int id);
         Task<List<ParkingModel>> Get();
+        Task<ParkingModel> Get(int id);
         Task<ParkingModel> Post(ParkingModel model);
         Task<ParkingModel> Put(ParkingModel model);
     }
