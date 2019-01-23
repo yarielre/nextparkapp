165cb3e759c2060c3b5aa85ae05274c76f5034b1
yariel rodriguez <yariel.re@wisegar.ch>
2019-01-23 18:06:07 +0100

    Restore online server url
    Conflicts:
    	Nextpark/NextPark.Mobile/Settings/ApiSettings.cs

--- Nextpark/NextPark.Mobile/Settings/ApiSettings.cs
+++ Nextpark/NextPark.Mobile/Settings/ApiSettings.cs
@@ -1,9 +1,13 @@
-﻿using System;
+using System;
 namespace NextPark.Mobile.Core.Settings
 {
     public static class ApiSettings
     {
+<<<<<<< HEAD
         public static string BaseUrl = "https://nextpark-api.azurewebsites.net"; // "http://10.211.55.3/NextPark.Api";// "https://nextpark-api.azurewebsites.net";
+=======
+        public static string BaseUrl =  "https://nextpark-api.azurewebsites.net"; //"http://10.211.55.3/NextPark.Api";
+>>>>>>> d242d12... Restore online server url
         //public static int BasePort = 80;
         //public static string BaseUrl = string.Format("{0}:{1}", BaseUri, BasePort);  
 
