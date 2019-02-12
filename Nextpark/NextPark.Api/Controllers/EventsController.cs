c9019453d6436cae3e958bcbea1845bbe183ca28
Yariel Rodriguez <yariel.re@wisegar.ch>
2019-02-12 21:42:16 +0100

    EventController Post endpoint update.

--- Nextpark/NextPark.Api/Controllers/EventsController.cs
+++ Nextpark/NextPark.Api/Controllers/EventsController.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using AutoMapper;
+﻿using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using NextPark.Data.Infrastructure;
 using NextPark.Data.Repositories;
 using NextPark.Domain.Entities;
 using NextPark.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace NextPark.Api.Controllers
 {
@@ -19,12 +16,14 @@ namespace NextPark.Api.Controllers
     public class EventsController : ControllerBase
     {
         private readonly IRepository<Event> _repository;
+        private readonly IRepository<Parking> _repositoryParking;
         private readonly IMapper _mapper;
         private readonly IUnitOfWork _unitOfWork;
 
-        public EventsController(IRepository<Event> repository, IUnitOfWork unitOfWork, IMapper mapper)
+        public EventsController(IRepository<Event> repository, IRepository<Parking> repositoryParking, IUnitOfWork unitOfWork, IMapper mapper)
         {
             _repository = repository;
+            _repositoryParking = repositoryParking;
             _mapper = mapper;
             _unitOfWork = unitOfWork;
         }
@@ -43,25 +42,41 @@ namespace NextPark.Api.Controllers
         public IActionResult Get(int id)
         {
             var entity = _repository.Find(id);
+
             if (entity == null)
                 return BadRequest("Entity not found");
+
             var vm = _mapper.Map<Event, EventModel>(entity);
+
             return Ok(vm);
         }
 
         // POST api/controller
         [HttpPost]
-        public async Task<IActionResult> Post([FromBody] Event entity)
+        public async Task<IActionResult> Post([FromBody] EventModel model)
         {
-             if (entity == null)
+             if (model == null)
                 return BadRequest("adding null entity");
+
             if (ModelState.IsValid)
             {
-                _repository.Add(entity);
+
+                var parking = _repositoryParking.Find(model.ParkingId);
+
+                if (parking == null)
+                    return BadRequest("Parking not found");
+
+                var eventEntity = _mapper.Map<EventModel, Event>(model);
+
+                _repository.Add(eventEntity);
+
                 await _unitOfWork.CommitAsync();
-                var vm = _mapper.Map<Event, EventModel>(entity);
+
+                var vm = _mapper.Map<Event, EventModel>(eventEntity);
+
                 return Ok(vm);
             }
+
             return BadRequest(ModelState);
         }
 
