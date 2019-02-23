ab9a1194b0ef1ba183188628da11a32146251816
yariel rodriguez <yariel.re@wisegar.ch>
2019-02-23 15:15:06 +0100

    Migrations updated

--- Nextpark/NextPark.Api/Migrations/20190223141350_changes.cs
+++ Nextpark/NextPark.Api/Migrations/20190223141350_changes.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace NextPark.Api.Migrations
 {
-    public partial class Changes : Migration
+    public partial class changes : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
