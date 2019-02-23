a4773516075799a27861c2863b8de778a8df441f
yariel rodriguez <yariel.re@wisegar.ch>
2019-02-23 13:35:43 +0100

    Data services and TestView

---
+++ Nextpark/NextPark.Mobile/Views/TestPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NextPark.Mobile.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
+using Xamarin.Forms.Xaml;
+
+namespace NextPark.Mobile.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TestPage : ContentPage
+    {
+        public TestPage()
+        {
+            InitializeComponent();
+            On<Xamarin.Forms.PlatformConfiguration.iOS>().SetUseSafeArea(true);
+
+            if (BindingContext == null) return;
+            if (BindingContext is BaseViewModel bvm)
+            {
+                bvm.InitializeAsync();
+            }
+        }
+    }
+}
