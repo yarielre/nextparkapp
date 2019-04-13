29b0580dd158962167f4d6823c3cd6b8b38f4424
Jaro <jaro.serena@gmail.com>
2019-04-13 07:36:33 +0200

    Revert "Revert "Fixes (To Be Checked) for issues  #28 and #29. Cash Transactions workflow added to the backend. Persistence included.""
    This reverts commit 14c4c4b2fcfc8574bbb18e1335556890fe6311c6.

---
+++ NextPark/NextPark.Models/Models/TransactionModel.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using NextPark.Enums.Enums;
+
+namespace NextPark.Models.Models
+{
+   public class TransactionModel:BaseModel
+    {
+        public string TransactionId { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime CompletationDate { get; set; }
+        public TransactionStatus Status { get; set; }
+        public TransactionType Type { get; set; }
+        public double CashMoved { get; set; }
+        public string  User { get; set; }
+        public int UserId { get; set; }
+        public string PurchaseId { get; set; }
+        public string PurchaseToken { get; set; }
+        public string PurchaseState { get; set; }
+
+        //Only for views purpose
+        public string TransactionStatus { get; set; }
+        public string TransactionType { get; set; }
+        public string CreationTime { get; set; }
+        public string CompleteTime { get; set; }
+    }
+}
