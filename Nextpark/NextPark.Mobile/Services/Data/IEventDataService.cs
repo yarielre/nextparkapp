c8cbd945138550a3e94dac7c5399db03f3dd353a
Jaro <jaro.serena@gmail.com>
2019-04-15 23:07:27 +0200

    Updated EventDataService
    * Update methods using ApiResponse with ErrorTypes

--- Nextpark/NextPark.Mobile/Services/Data/IEventDataService.cs
+++ Nextpark/NextPark.Mobile/Services/Data/IEventDataService.cs
@@ -7,10 +7,10 @@ namespace NextPark.Mobile.Services.Data
     public interface IEventDataService
     {
         Task<List<EventModel>> CreateEventAsync(EventModel model);
-        Task<EventModel> DeleteEventsAsync(int id);
-        Task<List<EventModel>> DeleteSerieEventsAsync(int id);
-        Task<EventModel> EditEventsAsync(int id, EventModel eventModel);
-        Task<List<EventModel>> EditSerieEventsAsync(EventModel eventModel);
+        Task<ApiResponse<EventModel>> DeleteEventsAsync(int id);
+        Task<ApiResponse<List<EventModel>>> DeleteSerieEventsAsync(int id);
+        Task<ApiResponse<EventModel>> EditEventsAsync(int id, EventModel eventModel);
+        Task<ApiResponse<List<EventModel>>> EditSerieEventsAsync(EventModel eventModel);
         Task<List<EventModel>> GetAllEventsAsync();
         Task<EventModel> GetEventAsync(int eventId);
     }
