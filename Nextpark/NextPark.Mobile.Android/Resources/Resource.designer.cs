96e997d27d50adc33907b0bceb565d5f484b07e4
Jaro <jaro.serena@gmail.com>
2019-07-04 23:57:22 +0200

    Added Android Notification Icon

--- Nextpark/NextPark.Mobile.Android/Resources/Resource.designer.cs
+++ Nextpark/NextPark.Mobile.Android/Resources/Resource.designer.cs
@@ -3532,44 +3532,44 @@ namespace NextPark.Mobile.Droid
 		public partial class Color
 		{
 			
-			// aapt resource value: 0x7f0b0058
-			public const int abc_background_cache_hint_selector_material_dark = 2131427416;
-			
 			// aapt resource value: 0x7f0b0059
-			public const int abc_background_cache_hint_selector_material_light = 2131427417;
+			public const int abc_background_cache_hint_selector_material_dark = 2131427417;
 			
 			// aapt resource value: 0x7f0b005a
-			public const int abc_btn_colored_borderless_text_material = 2131427418;
+			public const int abc_background_cache_hint_selector_material_light = 2131427418;
 			
 			// aapt resource value: 0x7f0b005b
-			public const int abc_btn_colored_text_material = 2131427419;
+			public const int abc_btn_colored_borderless_text_material = 2131427419;
 			
 			// aapt resource value: 0x7f0b005c
-			public const int abc_color_highlight_material = 2131427420;
+			public const int abc_btn_colored_text_material = 2131427420;
 			
 			// aapt resource value: 0x7f0b005d
-			public const int abc_hint_foreground_material_dark = 2131427421;
+			public const int abc_color_highlight_material = 2131427421;
 			
 			// aapt resource value: 0x7f0b005e
-			public const int abc_hint_foreground_material_light = 2131427422;
+			public const int abc_hint_foreground_material_dark = 2131427422;
+			
+			// aapt resource value: 0x7f0b005f
+			public const int abc_hint_foreground_material_light = 2131427423;
 			
 			// aapt resource value: 0x7f0b000c
 			public const int abc_input_method_navigation_guard = 2131427340;
 			
-			// aapt resource value: 0x7f0b005f
-			public const int abc_primary_text_disable_only_material_dark = 2131427423;
-			
 			// aapt resource value: 0x7f0b0060
-			public const int abc_primary_text_disable_only_material_light = 2131427424;
+			public const int abc_primary_text_disable_only_material_dark = 2131427424;
 			
 			// aapt resource value: 0x7f0b0061
-			public const int abc_primary_text_material_dark = 2131427425;
+			public const int abc_primary_text_disable_only_material_light = 2131427425;
 			
 			// aapt resource value: 0x7f0b0062
-			public const int abc_primary_text_material_light = 2131427426;
+			public const int abc_primary_text_material_dark = 2131427426;
 			
 			// aapt resource value: 0x7f0b0063
-			public const int abc_search_url_text = 2131427427;
+			public const int abc_primary_text_material_light = 2131427427;
+			
+			// aapt resource value: 0x7f0b0064
+			public const int abc_search_url_text = 2131427428;
 			
 			// aapt resource value: 0x7f0b000d
 			public const int abc_search_url_text_normal = 2131427341;
@@ -3580,29 +3580,29 @@ namespace NextPark.Mobile.Droid
 			// aapt resource value: 0x7f0b000f
 			public const int abc_search_url_text_selected = 2131427343;
 			
-			// aapt resource value: 0x7f0b0064
-			public const int abc_secondary_text_material_dark = 2131427428;
-			
 			// aapt resource value: 0x7f0b0065
-			public const int abc_secondary_text_material_light = 2131427429;
+			public const int abc_secondary_text_material_dark = 2131427429;
 			
 			// aapt resource value: 0x7f0b0066
-			public const int abc_tint_btn_checkable = 2131427430;
+			public const int abc_secondary_text_material_light = 2131427430;
 			
 			// aapt resource value: 0x7f0b0067
-			public const int abc_tint_default = 2131427431;
+			public const int abc_tint_btn_checkable = 2131427431;
 			
 			// aapt resource value: 0x7f0b0068
-			public const int abc_tint_edittext = 2131427432;
+			public const int abc_tint_default = 2131427432;
 			
 			// aapt resource value: 0x7f0b0069
-			public const int abc_tint_seek_thumb = 2131427433;
+			public const int abc_tint_edittext = 2131427433;
 			
 			// aapt resource value: 0x7f0b006a
-			public const int abc_tint_spinner = 2131427434;
+			public const int abc_tint_seek_thumb = 2131427434;
 			
 			// aapt resource value: 0x7f0b006b
-			public const int abc_tint_switch_track = 2131427435;
+			public const int abc_tint_spinner = 2131427435;
+			
+			// aapt resource value: 0x7f0b006c
+			public const int abc_tint_switch_track = 2131427436;
 			
 			// aapt resource value: 0x7f0b0010
 			public const int accent_material_dark = 2131427344;
@@ -3670,8 +3670,8 @@ namespace NextPark.Mobile.Droid
 			// aapt resource value: 0x7f0b0055
 			public const int colorPrimaryDark = 2131427413;
 			
-			// aapt resource value: 0x7f0b006c
-			public const int common_google_signin_btn_text_dark = 2131427436;
+			// aapt resource value: 0x7f0b006d
+			public const int common_google_signin_btn_text_dark = 2131427437;
 			
 			// aapt resource value: 0x7f0b0000
 			public const int common_google_signin_btn_text_dark_default = 2131427328;
@@ -3685,8 +3685,8 @@ namespace NextPark.Mobile.Droid
 			// aapt resource value: 0x7f0b0003
 			public const int common_google_signin_btn_text_dark_pressed = 2131427331;
 			
-			// aapt resource value: 0x7f0b006d
-			public const int common_google_signin_btn_text_light = 2131427437;
+			// aapt resource value: 0x7f0b006e
+			public const int common_google_signin_btn_text_light = 2131427438;
 			
 			// aapt resource value: 0x7f0b0004
 			public const int common_google_signin_btn_text_light_default = 2131427332;
@@ -3700,14 +3700,14 @@ namespace NextPark.Mobile.Droid
 			// aapt resource value: 0x7f0b0007
 			public const int common_google_signin_btn_text_light_pressed = 2131427335;
 			
-			// aapt resource value: 0x7f0b006e
-			public const int common_google_signin_btn_tint = 2131427438;
+			// aapt resource value: 0x7f0b006f
+			public const int common_google_signin_btn_tint = 2131427439;
 			
 			// aapt resource value: 0x7f0b0048
 			public const int design_bottom_navigation_shadow_color = 2131427400;
 			
-			// aapt resource value: 0x7f0b006f
-			public const int design_error = 2131427439;
+			// aapt resource value: 0x7f0b0070
+			public const int design_error = 2131427440;
 			
 			// aapt resource value: 0x7f0b0049
 			public const int design_fab_shadow_end_color = 2131427401;
@@ -3733,8 +3733,8 @@ namespace NextPark.Mobile.Droid
 			// aapt resource value: 0x7f0b0050
 			public const int design_snackbar_background_color = 2131427408;
 			
-			// aapt resource value: 0x7f0b0070
-			public const int design_tint_password_toggle = 2131427440;
+			// aapt resource value: 0x7f0b0071
+			public const int design_tint_password_toggle = 2131427441;
 			
 			// aapt resource value: 0x7f0b001e
 			public const int dim_foreground_disabled_material_dark = 2131427358;
@@ -3802,6 +3802,9 @@ namespace NextPark.Mobile.Droid
 			// aapt resource value: 0x7f0b0032
 			public const int material_grey_900 = 2131427378;
 			
+			// aapt resource value: 0x7f0b0058
+			public const int notification = 2131427416;
+			
 			// aapt resource value: 0x7f0b0051
 			public const int notification_action_color_filter = 2131427409;
 			
@@ -3859,11 +3862,11 @@ namespace NextPark.Mobile.Droid
 			// aapt resource value: 0x7f0b0042
 			public const int switch_thumb_disabled_material_light = 2131427394;
 			
-			// aapt resource value: 0x7f0b0071
-			public const int switch_thumb_material_dark = 2131427441;
-			
 			// aapt resource value: 0x7f0b0072
-			public const int switch_thumb_material_light = 2131427442;
+			public const int switch_thumb_material_dark = 2131427442;
+			
+			// aapt resource value: 0x7f0b0073
+			public const int switch_thumb_material_light = 2131427443;
 			
 			// aapt resource value: 0x7f0b0043
 			public const int switch_thumb_normal_material_dark = 2131427395;
@@ -6507,13 +6510,16 @@ namespace NextPark.Mobile.Droid
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int icon = 2130903040;
+			public const int ic_notification = 2130903040;
 			
 			// aapt resource value: 0x7f030001
-			public const int icon_round = 2130903041;
+			public const int icon = 2130903041;
 			
 			// aapt resource value: 0x7f030002
-			public const int launcher_foreground = 2130903042;
+			public const int icon_round = 2130903042;
+			
+			// aapt resource value: 0x7f030003
+			public const int launcher_foreground = 2130903043;
 			
 			static Mipmap()
 			{
