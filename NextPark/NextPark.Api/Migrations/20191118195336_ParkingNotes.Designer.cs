72de5cbb64ad85915f7af833a0cf03bee68f4889
Yariel Rodriguez <yariel.rodriguez@mtfbs.com>
2019-11-18 20:55:27 +0100

    Add parking entity migrations

--- NextPark/NextPark.Api/Migrations/20191118195336_ParkingNotes.Designer.cs
+++ NextPark/NextPark.Api/Migrations/20191118195336_ParkingNotes.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NextPark.Data;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -9,9 +10,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace NextPark.Api.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191118195336_ParkingNotes")]
+    partial class ParkingNotes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
